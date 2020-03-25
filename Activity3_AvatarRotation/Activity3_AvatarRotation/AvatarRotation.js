b2bc604d0ff115ca983a24ef39d02099fc6d4702
Rafael Rojas <yugnarcorp@hotmail.com>
2020-03-25 16:56:22 -0600

    Activity 3 upload & bug fixes

---
+++ Activity3_AvatarRotation/Activity3_AvatarRotation/AvatarRotation.js
@@ -0,0 +1,215 @@
+let renderer = null, 
+scene = null, 
+camera = null,
+root = null,
+group = null,
+control = null,
+objectList = [];
+
+let objLoader = null, jsonLoader = null;
+
+let duration = 20000; // ms
+let currentTime = Date.now();
+
+let directionalLight = null;
+let spotLight = null;
+let ambientLight = null;
+let pointLight = null;
+let mapUrl = "../images/checker_large.gif";
+
+let SHADOW_MAP_WIDTH = 2048, SHADOW_MAP_HEIGHT = 2048;
+// let objModelUrl = {obj:'../models/obj/Penguin_obj/penguin.obj', map:'../models/obj/Penguin_obj/peng_texture.jpg'};
+//let objModelUrl = {obj:'../models/obj/cerberus/Cerberus.obj', map:'../models/obj/cerberus/Cerberus_A.jpg', normalMap:'../models/obj/cerberus/Cerberus_N.jpg', specularMap: '../models/obj/cerberus/Cerberus_M.jpg'};
+let objModelUrl = {obj: '../models/obj/SimpleHuman1/basicman.obj', map:'../textures/humanText.jpg'};
+
+//let jsonModelUrl = { url:'../models/json/teapot-claraio.json' };
+
+function promisifyLoader ( loader, onProgress ) 
+{
+    function promiseLoader ( url ) {
+  
+      return new Promise( ( resolve, reject ) => {
+  
+        loader.load( url, resolve, onProgress, reject );
+  
+      } );
+    }
+  
+    return {
+      originalLoader: loader,
+      load: promiseLoader,
+    };
+}
+
+const onError = ( ( err ) => { console.error( err ); } );
+
+async function loadJson(url, objectList)
+{
+    const jsonPromiseLoader = promisifyLoader(new THREE.ObjectLoader());
+    
+    try {
+        const object = await jsonPromiseLoader.load(url);
+
+        object.castShadow = true;
+        object.receiveShadow = true;
+        object.position.y = -1;
+        object.position.x = 1.5;
+        object.name = "jsonObject";
+        objectList.push(object);
+        scene.add(object);
+
+    }
+    catch (err) {
+        return onError(err);
+    }
+}
+
+async function loadObj(objModelUrl, objectList)
+{
+    const objPromiseLoader = promisifyLoader(new THREE.OBJLoader());
+
+    try {
+        const object = await objPromiseLoader.load(objModelUrl.obj);
+        
+        let texture = objModelUrl.hasOwnProperty('map') ? new THREE.TextureLoader().load(objModelUrl.map) : null;
+        let normalMap = objModelUrl.hasOwnProperty('normalMap') ? new THREE.TextureLoader().load(objModelUrl.normalMap) : null;
+        let specularMap = objModelUrl.hasOwnProperty('specularMap') ? new THREE.TextureLoader().load(objModelUrl.specularMap) : null;
+
+        object.traverse(function (child) {
+            if (child instanceof THREE.Mesh) {
+                child.castShadow = true;
+                child.receiveShadow = true;
+                child.material.map = texture;
+                child.material.normalMap = normalMap;
+                child.material.specularMap = specularMap;
+            }
+        });
+
+        object.scale.set(0.8, 0.8, 0.8);
+        object.position.z = 3;
+        object.position.x = 0;
+        object.position.y = 2.55;
+        object.rotation.y = 0;
+        object.name = "objObject";
+        objectList.push(object);
+        scene.add(object);
+
+        control.attach( object );
+
+    }
+    catch (err) {
+        return onError(err);
+    }
+}
+
+function run() 
+{
+    requestAnimationFrame(function() { run(); });
+    
+    // Render the scene
+    render();
+
+}
+
+function setLightColor(light, r, g, b)
+{
+    r /= 255;
+    g /= 255;
+    b /= 255;
+    
+    light.color.setRGB(r, g, b);
+}
+
+function createScene(canvas) 
+{
+    // Create the Three.js renderer and attach it to our canvas
+    renderer = new THREE.WebGLRenderer( { canvas: canvas, antialias: true } );
+
+    // Set the viewport size
+    renderer.setSize(canvas.width, canvas.height);
+
+    // Turn on shadows
+    renderer.shadowMap.enabled = true;
+    // Options are THREE.BasicShadowMap, THREE.PCFShadowMap, PCFSoftShadowMap
+    renderer.shadowMap.type = THREE.BasicShadowMap;
+    
+    // Create a new Three.js scene
+    scene = new THREE.Scene();
+
+    // Add  a camera so we can view the scene
+    camera = new THREE.PerspectiveCamera( 45, canvas.width / canvas.height, 1, 4000 );
+    camera.position.set(0, 20, 30);
+    camera.lookAt(new THREE.Vector3(0,0,0));
+    
+    scene.add(camera);
+
+    //orbitControls = new THREE.OrbitControls(camera, renderer.domElement);
+    
+    // Create a group to hold all the objects
+    root = new THREE.Object3D;
+    
+    // Add a directional light to show off the object
+    directionalLight = new THREE.DirectionalLight( 0x000000, 1);
+
+    // Create and add all the lights
+    directionalLight.position.set(.5, 1, -3);
+    directionalLight.target.position.set(0,0,0);
+    directionalLight.castShadow = true;
+    root.add(directionalLight);
+
+    spotLight = new THREE.SpotLight (0x000000);
+    spotLight.position.set(2, 8, 15);
+    spotLight.target.position.set(-2, 0, -2);
+    root.add(spotLight);
+
+    spotLight.castShadow = true;
+
+    spotLight.shadow.camera.near = 1;
+    spotLight.shadow.camera.far = 200;
+    spotLight.shadow.camera.fov = 45;
+    
+    spotLight.shadow.mapSize.width = SHADOW_MAP_WIDTH;
+    spotLight.shadow.mapSize.height = SHADOW_MAP_HEIGHT;
+
+    ambientLight = new THREE.AmbientLight ( 0xffffff, 0.8);
+    root.add(ambientLight);
+    
+    // Create the objects
+    loadObj(objModelUrl, objectList);
+
+    // Create the TransformController
+
+    control = new THREE.TransformControls( camera, renderer.domElement );
+    control.addEventListener( 'change', render );
+    control.showX = ! control.showX;
+    control.showZ = ! control.showZ;
+    control.setMode( "rotate" );
+    scene.add( control );
+
+    // Create a group to hold the objects
+    group = new THREE.Object3D;
+    root.add(group);
+
+    // Create a texture map
+    let map = new THREE.TextureLoader().load(mapUrl);
+    map.wrapS = map.wrapT = THREE.RepeatWrapping;
+    map.repeat.set(8, 8);
+
+    let color = 0xffffff;
+
+    // Put in a ground plane to show off the lighting
+    let geometry = new THREE.PlaneGeometry(200, 200, 50, 50);
+    let mesh = new THREE.Mesh(geometry, new THREE.MeshPhongMaterial({color:color, map:map, side:THREE.DoubleSide}));
+
+    mesh.rotation.x = -Math.PI / 2;
+    mesh.position.y = -4.02;
+    mesh.castShadow = false;
+    mesh.receiveShadow = true;
+    group.add( mesh );
+
+    scene.add( root );
+}
+
+function render() {
+    renderer.render( scene, camera );
+}
